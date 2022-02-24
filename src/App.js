05b089be1e90b27aa9da8842b191fd204c197a44
Hoang Long <truonghoanglong305@gmail.com>
2022-02-24 09:47:17 +0700

    first second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import  Counter  from './redux-counter/components/Counter'
+import Counter from './redux-counter/components/Counter'
 import ModalScreen from './redux-modal/component/Modal'
 import Screen from './redux-modal/component/Screen'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Screen/>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            Screensss / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
